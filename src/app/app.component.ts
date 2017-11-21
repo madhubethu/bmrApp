b7bae81bf3b4bb3a4751ee758267c27493ac69cd
madhubethu <madhu_bethu@yahoo.co.in>
2017-11-21 22:30:37 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,4 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular updated on mypc 21-11-2017 22:23'; }
+export class AppComponent  { name = 'Angular updated on github 21-11-2017 22:30'; }
